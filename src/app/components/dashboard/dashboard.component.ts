bfe0b737146374d6e77bd2e9e333db50039673f7
Daniel Aldridge <daniel.aldridge@gmail.com>
2024-12-14 13:29:29 -0700

    Added dashboard service and dashboard rendering

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DashboardService } from '../../services/dashboard.service';
+import { Framework } from '../../models/frameworks.model';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  frameworks: Framework[] = [];
+  loading = true;
 
+  constructor(private frameworkService: DashboardService){
+
+    this.frameworkService.getFrameworks().subscribe({
+      next: (frameworks) => {
+        this.frameworks = frameworks;
+        this.loading = false;
+      },
+      error: (error) => {
+        console.error('Error fetching users:', error);
+        this.loading = false;
+      }
+    });
+
+
+  }
 }
